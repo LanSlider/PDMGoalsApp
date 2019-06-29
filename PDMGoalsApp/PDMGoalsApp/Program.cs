b09e3142c6492fe3759199fcbb745e3f921ded05
lanslider <dv.lanslider@gmail.com>
2019-06-30 01:02:54 +0300

    init project

---
+++ PDMGoalsApp/PDMGoalsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PDMGoalsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
